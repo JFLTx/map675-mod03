d2fc5d427d4f049945c835f43ec9e98580239b5a
James FL Thompson <jfth225@uky.edu>
2025-10-03 17:32:08 -0400

    try again to fix repo for GitHub Pages

--- map-libre-ls/vite.config.js
+++ map-libre-ls/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "./",
+  base: "/map675-mod03",
+  build: {
+    outDir: "docs",
+  },
   plugins: [tailwindcss()],
 });
